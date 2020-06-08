e89f18a7e874f45e539eec20230fa6348ccd0afe
EasyZero <eminem54@naver.com>
2020-06-08 23:55:26 +0900

    Typo: check

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -2,5 +2,10 @@ import React from "react";
 import ChartList from "./chartList";
 
 export default function Chart() {
-  return <ChartList />;
+  return (
+    <>
+      <ChartList />
+      <h1>태경아 이리 들어오도록 해</h1>
+    </>
+  );
 }
